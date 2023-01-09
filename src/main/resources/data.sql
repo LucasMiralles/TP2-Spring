1be0f9d9ec70f78003d4c29648b6ecc304bdf052
Lucas09M <lucas.miralles09@gmail.com>
2023-01-09 16:14:24 +0100

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Initialisation des tables
+INSERT INTO Country(code, name) VALUES
+    ('FR', 'France'), -- Les clés sont auto-générées
+    ('UK', 'United Kingdom'),
+    ('US', 'United States of America');
+
+INSERT INTO City(name, population, country_id) VALUES
+    ('Paris', 12, SELECT id FROM Country WHERE code = 'FR'), -- Les clés sont auto-générées
+    ('Londres', 10, SELECT id FROM Country WHERE code = 'UK'),
+    ('Dallas', 5, SELECT id FROM Country WHERE code = 'US');
